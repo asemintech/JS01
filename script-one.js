ff4f891db3e92a22481f470ee5abff1145583ba2
Asem <asemandakayeva@gmail.com>
2022-05-05 17:24:35 +0600

    Fixed code style JS01: HW

--- script-one.js
+++ script-one.js
@@ -1,11 +1,11 @@
-const numberOne = prompt('Please enter the first number:', 0);
-const numberTwo = prompt('Please enter the second number between 2 and 36:', 0);
-const numberTwoCheck = isNaN(numberTwo) || numberTwo < 2 || numberTwo > 36;
+const NUMBER_ONE = prompt('Please enter the first number:', 0);
+const NUMBER_TWO = prompt('Please enter the second number between 2 and 36:', 0);
+const NUMBER_TWO_CHECK = isNaN(NUMBER_TWO) || NUMBER_TWO < 2 || NUMBER_TWO > 36;
 
-if(isNaN(numberOne) || numberTwoCheck) {
+if (isNaN(NUMBER_ONE) || NUMBER_TWO_CHECK) {
     console.log('Некорректный ввод!'); 
 } else {
-    const result = Math.floor(numberOne).toString(numberTwo);
+    const result = Math.floor(NUMBER_ONE).toString(NUMBER_TWO);
     
     console.log(result); 
 }
--- script-two.js
+++ script-two.js
@@ -1,11 +1,11 @@
-const numberOne = prompt('Please enter the first number:', 0);
-const numberTwo = prompt('Please enter the second number:', 0);
+const NUMBER_ONE = prompt('Please enter the first number:', 0);
+const NUMBER_TWO = prompt('Please enter the second number:', 0);
 
-if(isNaN(numberOne) || isNaN(numberTwo)) { // + || numberTwo === 0
+if (isNaN(NUMBER_ONE) || isNaN(NUMBER_TWO)) { // || NUMBER_TWO === 0
     console.log('Некорректный ввод!');
 } else {
-    const resultOne = +numberOne + +numberTwo;
-    const resultTwo = numberOne / numberTwo;
+    const resultOne = +NUMBER_ONE + +NUMBER_TWO;
+    const resultTwo = NUMBER_ONE / NUMBER_TWO;
     
     console.log(`Answer: ${resultOne}, ${resultTwo}`);
 }
