8fbb95a1af6e7284b2dc988973209a3567e25a65
Asem <asemandakayeva@gmail.com>
2022-05-05 15:18:06 +0600

    Update JS01: HW

--- script-one.js
+++ script-one.js
@@ -1,7 +1,8 @@
 const numberOne = prompt('Please enter the first number:', 0);
 const numberTwo = prompt('Please enter the second number between 2 and 36:', 0);
+const numberTwoCheck = isNaN(numberTwo) || numberTwo < 2 || numberTwo > 36;
 
-if(isNaN(numberOne) || isNaN(numberTwo) || numberTwo < 2 || numberTwo > 36) {
+if(isNaN(numberOne) || numberTwoCheck) {
     console.log('Некорректный ввод!'); 
 } else {
     const result = Math.floor(numberOne).toString(numberTwo);
