263dc432d1ada802e0d127c8063a17c57cfa19ad
Asem <asemandakayeva@gmail.com>
2022-05-05 11:00:06 +0600

    Update JS01: HW

--- script-two.js
+++ script-two.js
@@ -1,11 +1,11 @@
-let numberOne = prompt('Please enter the first number:', 0);
-let numberTwo = prompt('Please enter the second number:', 0);
+const numberOne = prompt('Please enter the first number:', 0);
+const numberTwo = prompt('Please enter the second number:', 0);
 
 if(isNaN(numberOne) || isNaN(numberTwo)) { // + || numberTwo === 0
     console.log('Некорректный ввод!');
 } else {
-    let resultOne = +numberOne + +numberTwo;
-    let resultTwo = numberOne / numberTwo;
+    const resultOne = +numberOne + +numberTwo;
+    const resultTwo = numberOne / numberTwo;
     
     console.log(`Answer: ${resultOne}, ${resultTwo}`);
 }
